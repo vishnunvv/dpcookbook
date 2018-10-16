cd9df625bab96e81b3e6ec99ec075958d73dbf6a
Vishnu Narayanan <vishnuvnvv@gmail.com>
2018-10-16 10:17:10 +0000

    First Version

---
+++ cookbooks/starter/recipes/default.rb
@@ -0,0 +1,14 @@
+# This is a Chef recipe file. It can be used to specify resources which will
+# apply configuration to a server.
+
+log "Welcome to Chef, #{node["starter_name"]}!" do
+  level :info
+end
+
+# For more information, see the documentation: https://docs.chef.io/essentials_cookbook_recipes.html
+#
+#
+package "httpd"
+service "httpd" do
+         action : start
+end
---
+++ cookbooks/web/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: web
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+#
+#
+package "httpd"
+service "httpd" do
+         action:start
+end
